9e8366931ef5a7231a9bd5c1da7b91b4730fc74a
Frank Menezes <iamfrankallan@gmail.com>
2019-07-15 16:33:04 +0530

    Text change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. Feature Branch
+          New React.
         </a>
       </header>
     </div>
